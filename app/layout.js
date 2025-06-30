445191a1e34e53785c2113edf0d49edf053446a7
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-06-30 12:24:11 +0300

    Add Font and main Header

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
 import '@/app/_styles/globals.css'
 
-import Logo from '@/app/_components/Logo'
-import Navigation from '@/app/_components/Navigation'
+import { Josefin_Sans } from 'next/font/google'
+import Header from './_components/Header'
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: {
@@ -15,13 +20,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by the Wild Oasis</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 flex flex-col text-primary-100 min-h-screen`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   )
