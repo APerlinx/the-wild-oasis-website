5e13c19436dc160830a6cf9565b1f406fc72b9c6
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-06-30 17:39:49 +0300

    Add cabin details - dynamic segment

--- app/cabins/loading.js
+++ app/cabins/loading.js
@@ -3,7 +3,7 @@ import Spinner from '@/app/_components/Spinner'
 export default function LoadBindings() {
   return (
     <div className="grid items-center justify-center">
-      <p className="text-xl text-primary-200">Loading cabins...</p>
+      <p className="text-xl text-primary-200">Loading cabin...</p>
       <Spinner />
     </div>
   )
