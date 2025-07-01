d87acc3e547b336f1742a19f303a7fb5d3ccba19
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-07-01 12:56:53 +0300

    SSG cabin details

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -4,17 +4,17 @@ import logo from '@/public/logo.png'
 
 function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-4 z-10">
-      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
-      <Image
+    <Link href="/" className="flex items-center z-10">
+      {/* <Image
         src={logo}
         height="60"
         width="60"
         alt="The Wild Oasis logo"
         quality={100}
-      />
+      /> */}
+      <div className="h-16"></div>
 
-      <span className="text-xl font-semibold text-primary-100">
+      <span className="text-2xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
     </Link>
