7a7e5edaff18edb3d9690d67aa75c31d9856fd62
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-07-01 18:45:54 +0300

    Add text expander CC

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -36,7 +36,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
