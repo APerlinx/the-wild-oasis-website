5c0c64728daafda2d99b5a167616684e50af3968
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-07-03 16:59:28 +0300

    Add context for reservations

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,8 +1,10 @@
 'use client'
 
 import { isWithinInterval } from 'date-fns'
+import { useContext } from 'react'
 import { DayPicker } from 'react-day-picker'
 import 'react-day-picker/dist/style.css'
+import { useReservation } from './ReservationContext'
 
 function isAlreadyBooked(range, datesArr) {
   return (
@@ -15,12 +17,12 @@ function isAlreadyBooked(range, datesArr) {
 }
 
 function DateSelector({ settings, bookedDates, cabin }) {
+  const { range, setRange, resetRange } = useReservation()
   // CHANGE
   const regularPrice = 23
   const discount = 23
   const numNights = 23
   const cabinPrice = 23
-  const range = { from: null, to: null }
 
   // SETTINGS
 
@@ -31,6 +33,8 @@ function DateSelector({ settings, bookedDates, cabin }) {
       <DayPicker
         className="pt-12 place-self-center"
         mode="range"
+        onSelect={(range) => setRange(range)}
+        selected={range}
         min={minBookingLength + 1}
         max={maxBookingLength}
         startMonth={new Date()}
@@ -45,6 +49,7 @@ function DateSelector({ settings, bookedDates, cabin }) {
           range_start: 'bg-accent-600 text-white rounded-sm',
           range_end: 'bg-accent-600 text-white rounded-sm',
           range_middle: 'bg-accent-100 text-black',
+          hover_range: 'bg-accent-200 text-black',
         }}
         classNames={{
           chevron: 'fill-accent-500',
@@ -84,7 +89,7 @@ function DateSelector({ settings, bookedDates, cabin }) {
         {range.from || range.to ? (
           <button
             className="border border-primary-800 py-2 px-4 text-sm font-semibold"
-            onClick={() => resetRange()}
+            onClick={resetRange}
           >
             Clear
           </button>
