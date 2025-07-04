22d21f624fce3b48c346db2b1b16f7ecf5aa97d8
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-07-04 15:46:36 +0300

    Add user display to header and basic authorization to reservation

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,17 +1,24 @@
 import { getBookedDatesByCabinId, getSettings } from '@/app/_lib/data-service'
+import { auth } from '../_lib/auth'
 import DateSelector from './DateSelector'
 import ReservationForm from './ReservationForm'
+import LoginMessage from './LoginMessage'
 
 async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ])
+  const session = await auth()
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
       <DateSelector settings={settings} bookedDates={bookedDates} />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   )
 }
