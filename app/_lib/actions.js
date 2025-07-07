ac119c8788f3c8b0f67bbc5ed1128d0170c6ac38
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-07-07 12:03:45 +0300

    Add delete reservations

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -22,11 +22,20 @@ export async function updateGuest(formData) {
 
   const updateData = { nationality, countryFlag, nationalID }
 
-  const { data, error } = await supabase
+  const { error } = await supabase
     .from('guests')
     .update(updateData)
     .eq('id', session.user.guestId)
 
   if (error) throw new Error('Guest could not be updated')
   revalidatePath('/account/profile')
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth()
+  if (!session) throw new Error('You must be logged in')
+  const { error } = await supabase.from('bookings').delete().eq('id', bookingId)
+
+  if (error) throw new Error('Booking could not be deleted')
+  revalidatePath('/account/reservations')
 }
